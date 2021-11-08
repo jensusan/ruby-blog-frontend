079ffacb5ea68ce09fd76de60f83b18459316d52
Jennifer Susan <jennifersusan@Jennifers-MBP.cable.rcn.com>
2021-11-08 12:32:55 -0600

    connected front and back end

--- src/App.js
+++ src/App.js
@@ -2,8 +2,24 @@ import './App.css';
 import {route, routes} from 'react-router-dom';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [post, setPosts] = useState({posts: []});
+
+  const getPosts = async () => {
+    try {
+      const posts = await fetch('http://localhost:3000/posts').then(response => response.json())
+      console.log(posts)
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    getPosts()
+  }, []);
+
   return (
     <div className="App">
       <Header />
